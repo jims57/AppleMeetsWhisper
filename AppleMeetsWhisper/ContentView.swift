9b09929c4a1f1585dc4ab2f5eec26ff2d5d8d9ad
Arnav Singhal <arnavsinghal.developer@gmail.com>
2024-07-14 18:08:57 +0530

    Basic demo

--- AppleMeetsWhisper/ContentView.swift
+++ AppleMeetsWhisper/ContentView.swift
@@ -8,14 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var startImporting: Bool = false
+    @State var processing: Bool = false
+    @State var audioTranscription: String?
+    
+    var audioVM = AudioViewModel()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button("Import Audio") {
+                self.processing = false
+                self.audioTranscription = nil
+                self.startImporting.toggle()
+            }
+            
+            ScrollView {
+                if let audioTranscription {
+                    Text(audioTranscription)
+                        .multilineTextAlignment(.leading)
+                } else if processing {
+                    ProgressView()
+                }
+            }
         }
         .padding()
+        .fileImporter(isPresented: $startImporting, allowedContentTypes: [.audio]) { result in
+            handleFileMediaResponse(result)
+        }
+    }
+    
+    func handleFileMediaResponse(_ result: Result<URL, any Error>) {
+        switch result {
+            case .success(let success):
+                self.processing = true
+                _ = success.startAccessingSecurityScopedResource()
+                audioVM.extractTextFromAudio(success.absoluteURL) { result in
+                    switch result {
+                        case .success(let success):
+                            self.processing = false
+                            self.audioTranscription = success
+                        case .failure(let failure):
+                            print(failure)
+                    }
+                }
+            case .failure(let failure):
+                print(failure)
+        }
     }
 }
 
