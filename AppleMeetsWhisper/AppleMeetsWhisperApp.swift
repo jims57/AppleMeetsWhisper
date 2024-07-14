79ff12ed0b238239f26dfb60f12957109fdbcebf
Arnav Singhal <arnavsinghal.developer@gmail.com>
2024-07-14 17:53:59 +0530

    Initial Commit

---
+++ AppleMeetsWhisper/AppleMeetsWhisperApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMeetsWhisperApp.swift
+//  AppleMeetsWhisper
+//
+//  Created by Arnav Singhal on 14/07/24.
+//
+
+import SwiftUI
+
+@main
+struct AppleMeetsWhisperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppleMeetsWhisper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppleMeetsWhisper
+//
+//  Created by Arnav Singhal on 14/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
